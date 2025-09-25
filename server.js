60776deef2a11fd77b02dce5001ab501bcf7d9a3
guruias123 <guruias123@gmail.com>
2025-09-25 17:55:11 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const { Server } = require('socket.io');
+
+const server = http.createServer();
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+
+  socket.on('join-conversation', (conversationId) => {
+    socket.join(`conversation:${conversationId}`);
+  });
+
+  socket.on('leave-conversation', (conversationId) => {
+    socket.leave(`conversation:${conversationId}`);
+  });
+
+  socket.on('new-message', (message) => {
+    io.to(`conversation:${message.conversation_id}`).emit('message-received', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Socket server running on port ${PORT}`);
+});
